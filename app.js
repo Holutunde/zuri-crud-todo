433be8e6f1dc0c9b2abe7dac0377ebe965325370
Holutunde <Holutunde99@gmail.com>
2022-07-03 22:45:18 +0100

    notFound middleware

--- app.js
+++ app.js
@@ -1,6 +1,7 @@
 const express = require('express')
 const bodyParser = require('body-parser')
 const app = express()
+const notFound = require('./middleware/not-found')
 const tasks = require('./server/routes/tasks')
 const connectDB = require('./database/connect')
 require('dotenv').config()
@@ -13,6 +14,7 @@ app.use(express.json())
 // routes
 app.use('/tasks', tasks)
 
+app.use(notFound)
 const port = process.env.PORT || 5000
 
 const start = async () => {
