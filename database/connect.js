f9308be1e98ce3eaaafb6ec6fc6659276d4f216d
Holutunde <Holutunde99@gmail.com>
2022-07-08 13:39:33 +0100

    change connection to Atlas

--- database/connect.js
+++ database/connect.js
@@ -2,6 +2,8 @@ const mongoose = require('mongoose')
 
 const connectDB = (url) => {
   return mongoose.connect(url)
+  .then(console.log("connected to Database"))
+  .catch((err) => console.log(err))
 }
 
 module.exports = connectDB
